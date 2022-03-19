16099dadc35fb4f25dbc6b848d48eca5d0aef65f
Miguel(MgARL) <mgarlcr@gmail.com>
2022-03-19 13:26:28 -0400

    Added Styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import './App.css';
+import Example from './components/Example';
+import StyleContext from './components/StyleContext';
 
 function App() {
+  const styledText = {
+    fontSize: "25px",
+    textAlign: "center",
+    margin: "40px",
+    border: "2px solid green"
+  };
+
+  const styledLink = {
+    textDecoration: 'none',
+    fontSize: '20px'
+  }
+
+
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <StyleContext.Provider value={[styledText, styledLink]}>
+      <div className="App">
+        <h1>Hello World</h1>
+        <Example />
+      </div>
+    </StyleContext.Provider>
   );
 }
 
